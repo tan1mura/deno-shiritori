94331cd115a2235b42c1121c058cd9873a016a88
Tan1mura <r119321@g.kobe-kosen.ac.jp>
2022-06-13 12:34:33 +0900

    test

---
+++ server.js
@@ -0,0 +1,32 @@
+import { serve } from "https://deno.land/std@0.138.0/http/server.ts";
+import { serveDir } from "https://deno.land/std@0.138.0/http/file_server.ts";
+
+console.log("Listening on http://localhost:8000");
+const previousWord = "しりとり";
+serve(async (req) => {
+  const pathname = new URL(req.url).pathname;
+  if (req.method === "GET" && pathname === "/shiritori") {
+    return new Response(previousWord);
+  }
+  if (req.method === "POST" && pathname === "/shiritori") {
+    const requestJson = await req.json();
+    const nextWord = requestJson.nextWord;
+    if (
+      nextWord.length > 0 &&
+      previousWord.charAt(previousWord.length - 1) !== nextWord.charAt(0)
+    ) {
+      return new Response("前の単語に続いていません。", { status: 400 });
+    }
+    if(nextWord.slice(-1) === "ん"){
+      return new Response("君の負け", { status: 400});
+    }
+    previousWord = nextWord;
+    return new Response(previousWord);
+  }
+  return serveDir(req, {
+    fsRoot: "public",
+    urlRoot: "",
+    showDirListing: true,
+    enableCors: true,
+  });
+});
